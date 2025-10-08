4492e3a1daf3ff1833dd6c24ac5c606645cc2cf9
Jubaer Ahmed Faysal <157054403+JubaerFaysal@users.noreply.github.com>
2025-10-08 13:38:57 +0600

    Jubear Commit

---
+++ first.js
@@ -0,0 +1,39 @@
+// let a=["java","c++","python"];
+// let b=["html","css","js"];
+
+// b=a;
+
+// console.log(a);
+// console.log(b);
+
+// a.push("sql");
+
+// console.log(a);
+// console.log(b);
+
+// b.push("react");
+
+// console.log(a);
+// console.log(b);
+
+// let fruits=["apple","banana","grapes"];
+
+// let ff=[];
+
+// fruits.find((f)=>{
+//     if(f==="apple")ff.push(f);
+// });
+// console.log(ff);
+
+let num=[1,2,3,4,5,6,7,8,9];
+
+const result=num.reduce((total,currVal)=>(
+    total+currVal),0)
+
+console.log(result);
+
+let user = {
+    name: "john",
+    age: 30,
+    city: "new york"
+}
---
+++ type.ts
@@ -0,0 +1,5 @@
+let users: { name: string, age: number, city: number } = {
+    name: "John",
+    age: 30,
+    city: 123
+}
